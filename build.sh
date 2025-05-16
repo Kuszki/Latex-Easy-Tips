dbe7c8373cf1a55565ce7d8845627f048769491d
Kuszki <l.drozdz@o2.pl>
2025-05-16 20:20:21 +0200

    1st part

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Konwertuj pliki 'odg' z folderu 'obrazki' do formatu 'pdf'
+[ -f obrazki/*.odg ] && libreoffice --convert-to pdf obrazki/*.odg --outdir obrazki
+
+# Konwertuj pliki 'svg' z folderu 'obrazki' do formatu 'pdf'
+[ -f obrazki/*.svg ] && inkscape -D obrazki/*.svg --export-type pdf
+
+# Uruchom kompilacje dokumentu i zapisz wyniki do katalogu 'budowa'
+latexmk --shell-escape -output-directory=budowa -pdflua -f thesis.tex
